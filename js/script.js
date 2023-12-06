c911801dc933c959ca95051ef84fa3c68a919daa
Roofi <roofiraditya07@gmail.com>
2023-12-06 16:02:09 +0800

    first push

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function calculate() {
+    var sideA = parseFloat(document.getElementById("sideA").value);
+    var sideB = parseFloat(document.getElementById("sideB").value);
+    var sideC = parseFloat(document.getElementById("sideC").value);
+
+    if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
+        alert("Masukkan panjang sisi dengan angka!");
+        return;
+    }
+
+    var perimeter = sideA + sideB + sideC;
+    var s = perimeter / 2; // semi-perimeter
+    var area = Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+
+    document.getElementById("resultArea").innerText = area.toFixed(2);
+    document.getElementById("resultPerimeter").innerText = perimeter.toFixed(2);
+}
